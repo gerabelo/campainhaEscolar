8dde6107f88b9eabcc378d1dc77c43af6020a0f1
Geraldo Rabelo <geraldo.rabelo@gmail.com>
2019-02-05 09:55:58 -0300

    Update campainha.c

--- campainha.c
+++ campainha.c
@@ -12,6 +12,7 @@
 	echo 1 > /dev/ttyACM0
 	echo 2 > /dev/ttyACM0
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -41,56 +42,72 @@ int main(void) {
 	alarme[1].minutos = 45;
 	alarme[1].status = 0;
 
+
 	alarme[2].hora = 9;
 	alarme[2].minutos = 5;
 	alarme[2].status = 0;
+
 
 	alarme[3].hora = 9;
 	alarme[3].minutos = 15;
 	alarme[3].status = 0;
 
+
 	alarme[4].hora = 9;
 	alarme[4].minutos = 30;
 	alarme[4].status = 0;
+
 
 	alarme[5].hora = 11;
 	alarme[5].minutos = 0;
 	alarme[5].status = 0;
 
+
 	alarme[6].hora = 13;
 	alarme[6].minutos = 0;
 	alarme[6].status = 0;
+
 
 	alarme[7].hora = 14;
 	alarme[7].minutos = 45;
 	alarme[7].status = 0;
 
+
 	alarme[8].hora = 15;
 	alarme[8].minutos = 5;
 	alarme[8].status = 0;
+
 
 	alarme[9].hora = 15;
 	alarme[9].minutos = 15;
 	alarme[9].status = 0;
 
+
 	alarme[10].hora = 15;
 	alarme[10].minutos = 30;
 	alarme[10].status = 0;
 
+
 	alarme[11].hora = 17;
-	alarme[11].minutos = 0;
+	alarme[11].minutos = 00;
 	alarme[11].status = 0;
 
 	while(1) {
+
 		if (i == 12) { i = 0; }
 		if (alarme[i].status == 1) {
 			printf("\n[%02d:%02d] proximo alarme: %02d:%02d",hora_atual->tm_hour,hora_atual->tm_min,alarme[i].hora,alarme[i].minutos);
-			while(1) {
+			while(alarme[i].status == 1) {
+				printf(".");
+				time(&now);
+				hora_atual = localtime(&now);
 				if ((alarme[i].hora == hora_atual->tm_hour) && (alarme[i].minutos == hora_atual->tm_min)) {
 					alarme[i].status = 0;
 					soar();
+					sleep(65);
+					i++;
 				}
-				sleep(30);
+				sleep(5);
 			}
 		}
 
@@ -106,14 +123,16 @@ int main(void) {
 	}
 }
 
+
 void soar(void) {
 	time_t now;
 	time(&now);
 	struct tm *hora_atual = localtime(&now);
 
 	printf("\n[%2d:%2d] Soando...\n",hora_atual->tm_hour,hora_atual->tm_min);
-	system("MODE COM1:9600,N,8,1");
-	system("ECHO 1 > COM1");
-	sleep(3);
-	system("ECHO 2 > COM1");
+	system("MODE COM3:9600,N,8,1");
+	sleep(1);
+	system("echo '1' > COM3");
+	sleep(4);
+	system("echo '2' > COM3");
 }
